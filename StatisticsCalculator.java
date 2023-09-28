5667724d0c1817f62136de09031558cf9568108c
Abdulaziz <azizn890@gmail.com>
2023-09-28 10:31:18 +0000

    Initial commit

---
+++ StatisticsCalculator.java
@@ -0,0 +1,36 @@
+import java.util.List;
+import java.util.Collections;
+
+class StatisticsCalculator {
+
+    public double calculateMean(List<Integer> numbers) {
+        if (numbers == null || numbers.isEmpty()) {
+            throw new IllegalArgumentException("List cannot be null or empty");
+        }
+
+        double sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+
+        return sum / numbers.size();
+    }
+
+    public double calculateMedian(List<Integer> numbers) {
+        if (numbers == null || numbers.isEmpty()) {
+            throw new IllegalArgumentException("List cannot be null or empty");
+        }
+
+        List<Integer> sortedNumbers = numbers;
+        Collections.sort(sortedNumbers);
+
+        int middleIndex = sortedNumbers.size() / 2;
+        if (sortedNumbers.size() % 2 == 0) {
+            // even size
+            return (sortedNumbers.get(middleIndex - 1) + sortedNumbers.get(middleIndex)) / 2.0;
+        } else {
+            // odd size
+            return sortedNumbers.get(middleIndex);
+        }
+    }
+}
